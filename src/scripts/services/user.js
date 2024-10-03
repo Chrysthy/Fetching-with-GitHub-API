c86f454b0b69d7a4a027bd884c1303f286bd9368
Chrystine Martins <chrystine.martins@outlook.com>
2024-10-03 19:03:39 -0300

    update function name

--- src/scripts/services/user.js
+++ src/scripts/services/user.js
@@ -1,6 +1,6 @@
 import { baseUrl } from "/src/scripts/variables.js"
 
-async function user(userName) {
+async function getUser(userName) {
 
     const response = await fetch(`${baseUrl}/${userName}`)
 
@@ -8,4 +8,4 @@ async function user(userName) {
 
 }
 
-export { user }
+export { getUser }
