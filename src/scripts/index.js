da7d7aff375d5822e460654620a2a2c64172b830
Chrystine Martins <chrystine.martins@outlook.com>
2024-10-02 21:30:49 -0300

    remove user and repos function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -18,21 +18,6 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
     }
 })
 
-async function user(userName) {
-
-    const response = await fetch(`${baseUrl}/${userName}`)
-
-    return await response.json()
-
-}
-
-async function repos(userName) {
-
-    const response = await fetch(`${baseUrl}/${userName}/repos?per_page=${repositoriesQuantity}`)
-
-    return await response.json()
-
-}
 
 function getUserProfile(userName) {
 
