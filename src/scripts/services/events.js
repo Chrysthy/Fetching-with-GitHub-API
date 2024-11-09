a068dae46816bfe264fff70a55a26d207c34b72e
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-09 17:31:53 -0300

    update getEvents function

--- src/scripts/services/events.js
+++ src/scripts/services/events.js
@@ -1,13 +1,12 @@
 import { baseUrl, eventsQuantity } from "../variables.js"
 
 async function getEvents(userName) {
-
     const response = await fetch(`${baseUrl}/${userName}/events`);
-
     const events = await response.json();
 
-    return events.filter(element => element.type === "CreateEvent" || element.type === "PushEvent").sloce(0, eventsQuantity);
+    const filteredEvents = events.filter(event => event.type === 'PushEvent' || event.type === 'CreateEvent');
 
+    return filteredEvents.slice(0, 10);
 }
 
-export { getEvents }
+export { getEvents };
